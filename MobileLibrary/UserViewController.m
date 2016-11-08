6024a4f6f56019e701dff501101ef480374152bd
BIBIN K ONANKUNJU <bibin.onankunju@gmail.com>
2016-11-08 11:23:12 +0530

    Modified User View

--- MobileLibrary/UserViewController.m
+++ MobileLibrary/UserViewController.m
@@ -21,11 +21,62 @@ float sdwidth,sdheight;
 
     self.view.backgroundColor = [UIColor lightGrayColor];
 
+    [self loadContents];
+    
+}
 
-    UIButton *lgoutBtn = [[UIButton alloc]initWithFrame:CGRectMake(sdwidth/3+sdwidth/6+4, sdheight/3 + 100, sdwidth/6 - 2, 25)];
+-(void)loadContents{
+    
+    NSArray *itemArray = [NSArray arrayWithObjects: @"Home", @"Books", @"Profile", nil];
+    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:itemArray];
+    [segmentedControl addTarget:self action:@selector(MySegmentControlAction:) forControlEvents: UIControlEventValueChanged];
+    segmentedControl.selectedSegmentIndex = 0;
+    segmentedControl.tintColor = [UIColor whiteColor];
+    [self.view addSubview:segmentedControl];
+    
+    CGRect segframe=segmentedControl.frame;
+    segframe.size.width = sdwidth * 0.8;
+    segframe.origin.x = sdwidth * 0.1;
+    segframe.origin.y = 50;
+    segmentedControl.frame=segframe;
+    
+    UIButton *lgoutBtn = [[UIButton alloc]initWithFrame:CGRectMake(sdwidth * 0.2, sdheight - 30, sdwidth * 0.6, 30)];
     [lgoutBtn setTitle:@"LogOut" forState:UIControlStateNormal];
     [lgoutBtn addTarget:self action:@selector(LogOut_BtnClk:) forControlEvents:UIControlEventTouchDown];
     [self.view addSubview:lgoutBtn];
+    
+}
+
+-(void)MySegmentControlAction : (UISegmentedControl*) sender
+{
+    switch (sender.selectedSegmentIndex)
+    {
+        case 0:
+            [self MainView];
+            break;
+        case 1:
+            [self BookView];
+            break;
+        case 2:
+            [self ProfileView];
+             break;
+            
+        default:
+            break;
+            
+    }
+}
+
+-(void) MainView{
+    
+}
+
+-(void) BookView{
+    
+}
+
+-(void) ProfileView{
+    
 }
 
 - (IBAction)LogOut_BtnClk:(id)sender {
